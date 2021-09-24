837c37e2f73f37fd7701e269cbffe2d078f014a6
tan-weijie <tanweijie.91@gmail.com>
2021-09-24 15:01:54 +0800

    created Navbar

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbar/>
+
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = (props) => {
+    return (
+        <div className='navbar'>
+            <h1>Nav bar</h1>
+                <button onClick="">Home</button>
+                <button onClick="">Add Item</button>
+                <button onClick="">Profile</button>
+                <button onClick="">Sign Out</button>
+        </div>
+    )
+}
+
+export default Navbar;
